6d55c1d66203c3806859ca4f8eea83c1d0fa5138
KayVee <kalai.v.vani@gmail.com>
2025-12-01 08:13:25 +0530

    added dynamic route

---
+++ app/blog/[id].js
@@ -0,0 +1,18 @@
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
+import { Pressable, Text, View } from "react-native";
+
+export default function Blog() {
+    const router = useRouter();
+    const { id } = useLocalSearchParams();   
+    return (
+        <>
+        <Stack.Screen options={{ title: `Blog Post ${id}` }} />
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Blog page {id}</Text>
+        <Pressable onPress={() => router.back()} style={{padding: 10, backgroundColor: 'lightblue', borderRadius: 5, marginTop: 10}}>
+        <Text>Back to Home</Text>
+        </Pressable>
+        </View>
+        </>
+    );
+}
--- app/blog/index.js
+++ app/blog/index.js
@@ -6,6 +6,13 @@ export default function Blog() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Blog page</Text>
+      
+      <Pressable onPress={() => router.push("/blog/1")} style={{padding: 10, backgroundColor: 'lightblue', borderRadius: 5, marginTop: 10}}>
+        <Text>Blog post 1</Text>
+      </Pressable>
+      <Pressable onPress={() => router.push("/blog/2")} style={{padding: 10, backgroundColor: 'lightblue', borderRadius: 5, marginTop: 10}}>
+        <Text>Blog post 2</Text>
+      </Pressable>
       <Pressable onPress={() => router.back()} style={{padding: 10, backgroundColor: 'lightblue', borderRadius: 5, marginTop: 10}}>
         <Text>Back to Home</Text>
       </Pressable>
