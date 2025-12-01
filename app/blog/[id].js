98257dad44c3e88b89856665f704c1edde79033f
KayVee <kalai.v.vani@gmail.com>
2025-12-01 08:22:17 +0530

    conditional rendering of author

--- app/blog/[id].js
+++ app/blog/[id].js
@@ -3,12 +3,17 @@ import { Pressable, Text, View } from "react-native";
 
 export default function Blog() {
     const router = useRouter();
-    const { id } = useLocalSearchParams();   
+    const { id, author } = useLocalSearchParams();   
     return (
         <>
         <Stack.Screen options={{ title: `Blog Post ${id}` }} />
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Blog page {id}</Text>
+        <Text style={{marginBottom: 20,fontSize: 18}}>Blog page {id}</Text>
+        { author && 
+        ( 
+        <Text style={{marginBottom: 20,fontSize: 16}}>Author: {author}</Text>
+        ) }
+
         <Pressable onPress={() => router.back()} style={{padding: 10, backgroundColor: 'lightblue', borderRadius: 5, marginTop: 10}}>
         <Text>Back to Home</Text>
         </Pressable>
--- app/blog/index.js
+++ app/blog/index.js
@@ -10,7 +10,7 @@ export default function Blog() {
       <Pressable onPress={() => router.push("/blog/1")} style={{padding: 10, backgroundColor: 'lightblue', borderRadius: 5, marginTop: 10}}>
         <Text>Blog post 1</Text>
       </Pressable>
-      <Pressable onPress={() => router.push("/blog/2")} style={{padding: 10, backgroundColor: 'lightblue', borderRadius: 5, marginTop: 10}}>
+      <Pressable onPress={() => router.push("/blog/2?author=John")} style={{padding: 10, backgroundColor: 'lightblue', borderRadius: 5, marginTop: 10}}>
         <Text>Blog post 2</Text>
       </Pressable>
       <Pressable onPress={() => router.back()} style={{padding: 10, backgroundColor: 'lightblue', borderRadius: 5, marginTop: 10}}>
